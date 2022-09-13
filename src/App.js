3d2976746b68eb4842d44d74589cee7b349a48e0
Tomás Rossi <91207352+rossitom90@users.noreply.github.com>
2022-09-13 20:31:37 -0300

    install tailwind

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import saludos from './components/Saludo';
 
 function App() {
   return (
-    <>
-   <h1>Hola mundo!!!</h1>
-    <h2>Tomas Rossi</h2>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
-    <saludos></saludos>
-   </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
--- src/components/Saludo.js
+++ src/components/Saludo.js
@@ -1,10 +1,9 @@
 function saludos() {
     return(
-    <>
+    
     <h1>Saludos!!</h1>
     
-    </>
-    );
+    )
 }
 
 export default saludos
