b5a1cfe8c82a3d7deac3ba3646fec5adfa80f4d3
Tomás Rossi <91207352+rossitom90@users.noreply.github.com>
2022-09-18 19:09:11 -0300

    update errorBody

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Saludos from './components/Saludo';
 import BoxForChildren from './components/BoxForChildren';
-import Example from './components/NavBar';
+import NavBar from './components/NavBar';
+import bodyProductos from './components/bodyProductos';
 
 function App() {
   return (
 <>
-<Example></Example>
+<NavBar></NavBar>
+<bodyProductos></bodyProductos>
 
-<h1>Hola Mundo!!</h1>
-    <Saludos name='Tomas' age={32} />
-    <Saludos name='Cecilia' age={29} />
-    <Saludos name='Ezequiel' age={35} />
-    <Saludos name='Martin' age={28} />
-
-    <BoxForChildren></BoxForChildren>
 </> 
 
   );
